03973d465c0c6166f218cd431a0eead997c531ac
utii6 <utii6@iCloud.com>
2025-08-31 01:51:05 +0300

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,39 @@
+import json
+from telegram import ContextTypes
+
+# -------------------------
+# نقاط المستخدمين
+# -------------------------
+try:
+    with open("points.json", "r") as f:
+        points = json.load(f)
+except:
+    points = {}
+
+def save_points():
+    with open("points.json", "w") as f:
+        json.dump(points, f, indent=4)
+
+# -------------------------
+# المستخدمين
+# -------------------------
+try:
+    with open("users.json", "r") as f:
+        users = json.load(f)
+except:
+    users = []
+
+def save_users():
+    with open("users.json", "w") as f:
+        json.dump(users, f, indent=4)
+
+# -------------------------
+# التحقق من الاشتراك في القناة
+# -------------------------
+async def check_subscription(user_id, context: ContextTypes.DEFAULT_TYPE):
+    CHANNEL_ID = "@qd3qd"  # ضع هنا معرف قناتك
+    try:
+        member = await context.bot.get_chat_member(CHANNEL_ID, user_id)
+        return member.status in ["member", "administrator", "creator"]
+    except:
+        return False
