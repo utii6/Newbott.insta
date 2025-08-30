2cb71828b49d8ede9d3fd44fbf244a4e662e0798
utii6 <utii6@iCloud.com>
2025-08-31 02:03:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
-{
-  "bot_token": "6663550850:AAFnTssn67o5Ba99eoTUiLQMQPL6F_qMbo4",
-  "api_key": "81db6d6480686d9da6f35ff2cf6a30b4",
-  "admin_id": 5581457665,
-  "api_url": "https://kd1s.com/api/v2",
-  "channel_id": "@qd3qd",
-  "min_order": 50,
-  "max_order": 10000
-}
+BOT_TOKEN =  "6663550850:AAFnTssn67o5Ba99eoTUiLQMQPL6F_qMbo4"
+API_KEY = "81db6d6480686d9da6f35ff2cf6a30b4"
+ADMIN_ID = 5581457665
+API_URL = "https://kd1s.com/api/v2"
+CHANNEL_ID = "@qd3qd"
+MIN_ORDER = 50
+MAX_ORDER = 10000
+START_POINTS = 100
